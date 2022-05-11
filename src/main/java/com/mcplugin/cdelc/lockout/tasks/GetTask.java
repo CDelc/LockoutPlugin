6e5ffa773510d9d63316ac622fcef96c35c3d400
hdelc02 <hdelc02@gmail.com>
2022-05-10 21:05:42 -0400

    Add GetTask file

---
+++ src/main/java/com/mcplugin/cdelc/lockout/tasks/GetTask.java
@@ -0,0 +1,16 @@
+package com.mcplugin.cdelc.lockout.tasks;
+
+import com.mcplugin.cdelc.lockout.GameInstance;
+import org.bukkit.event.Event;
+
+public class GetTask extends Task {
+
+    public GetTask(GameInstance game) {
+        super(game);
+    }
+
+    @Override
+    public void onEvent(Event e) {
+        
+    }
+}
